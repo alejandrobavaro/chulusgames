32ca269667bc14d14b96a4e56c53b4db8dd72cd1
alejandrobavaro <bavaroalejandro@gmail.com>
2024-10-05 21:41:22 -0300

    cambie links

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './src', // Define que el directorio raíz sea src
   plugins: [react()],
-  build: {
-    outDir: '../build', // Opcional, si deseas cambiar el directorio de salida
-    emptyOutDir: true,
-  },
-});
+})
