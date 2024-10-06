b98b525a4bb49aa866350c89445edbc092b3908f
alejandrobavaro <bavaroalejandro@gmail.com>
2024-10-05 21:31:48 -0300

    cambie el archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: './src', // Define que el directorio raíz sea src
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    open: true,
+    outDir: '../build', // Opcional, si deseas cambiar el directorio de salida
+    emptyOutDir: true,
   },
 });
