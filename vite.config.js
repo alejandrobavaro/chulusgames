5129ad22d76c3aaad4c207f0c57850b9c6c7b0ea
alejandrobavaro <bavaroalejandro@gmail.com>
2024-10-13 16:43:51 -0300

    subiendo ultimos cambios antes de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Cambié el directorio de salida de 'dist' a 'build'
+  },
+  server: {
+    port: 3000, // Opcional: Cambia el puerto si lo deseas
+    open: true, // Abre automáticamente el navegador al iniciar el servidor de desarrollo
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Alias opcional para acortar rutas de importación
+    },
+  },
+});
