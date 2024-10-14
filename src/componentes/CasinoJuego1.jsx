46f20d8308316d9164992594140ada2996f9926c
alejandrobavaro <bavaroalejandro@gmail.com>
2024-10-13 22:03:59 -0300

    actualizando imagenes 1

--- src/componentes/CasinoJuego1.jsx
+++ src/componentes/CasinoJuego1.jsx
@@ -1,93 +1,28 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "../assets/scss/_03-Componentes/_CasinoJuego1.scss";
 
 const CasinoJuego1 = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-  };
-
-  const banners = [
-    "/img/04-img-banners/banner1.png",
-    "/img/04-img-banners/banner2.png",
-    "/img/04-img-banners/banner3.png",
-    "/img/04-img-banners/banner4.png",
-    "/img/04-img-banners/banner5.png",
-    "/img/04-img-banners/banner6.png",
-    "/img/04-img-banners/banner7.png",
-    "/img/04-img-banners/banner8.png",
-    "/img/04-img-banners/banner9.png",
-    "/img/04-img-banners/banner10.png",
-    "/img/04-img-banners/banner11.png",
-    "/img/04-img-banners/banner12.png",
-    "/img/04-img-banners/banner13.png",
-    "/img/04-img-banners/banner14.png",
-    "/img/04-img-banners/banner15.png",
-    "/img/04-img-banners/banner16.png",
-    "/img/04-img-banners/banner17.png",
-    "/img/04-img-banners/banner18.png",
-    "/img/04-img-banners/banner19.png",
-    "/img/04-img-banners/banner20.png",
-    "/img/04-img-banners/banner21.png",
-    "/img/04-img-banners/banner22.png",
-    "/img/04-img-banners/banner23.png",
-    "/img/04-img-banners/banner24.png",
-    "/img/04-img-banners/banner25.png",
-    "/img/04-img-banners/banner26.png",
-    "/img/04-img-banners/banner27.png",
-    "/img/04-img-banners/banner28.png",
-    "/img/04-img-banners/banner29.png",
-    "/img/04-img-banners/banner30.png",
-    "/img/04-img-banners/banner31.png",
+  const games = [
+    { name: "Blackjack Royale", img: "/img/casino-games/blackjack-royale.png" },
+    { name: "Slots Deluxe", img: "/img/casino-games/slots-deluxe.png" },
+    { name: "Poker Legends", img: "/img/casino-games/poker-legends.png" },
+    { name: "Roulette Master", img: "/img/casino-games/roulette-master.png" },
+    { name: "Baccarat Pro", img: "/img/casino-games/baccarat-pro.png" },
+    { name: "Craps Supreme", img: "/img/casino-games/craps-supreme.png" },
+    { name: "Lucky Keno", img: "/img/casino-games/lucky-keno.png" },
+    { name: "Texas Hold'em", img: "/img/casino-games/texas-holdem.png" },
   ];
 
   return (
-    <div className="gridPadrePublicidad1">
-      <div className="publicidad-grid-contenedor">
-        <div className="publicidad-grid fila-publicidad">
-          <div className="publicidad-container">
-            <Slider {...settings}>
-              {banners.map((banner, index) => (
-                <div key={index} className="slide-item">
-                  <img
-                    src={banner}
-                    alt={`Banner ${index + 1}`}
-                    className="imagen-publicidad objetoCentrado1"
-                  />
-    
-                </div>
-                
-              ))}
-            </Slider>
+    <div className="casino-grid-container">
+      <h2 className="casino-title">Featured Casino Games</h2>
+      <div className="casino-grid">
+        {games.map((game, index) => (
+          <div key={index} className="casino-card">
+            <img src={game.img} alt={game.name} className="casino-game-img" />
+            <h3 className="casino-game-title">{game.name}</h3>
           </div>
-          <div className="publicidad-container">
-            <Slider {...settings}>
-              {banners
-                .slice()
-                .reverse()
-                .map((banner, index) => (
-                  <div key={index} className="slide-item">
-                    <img
-                      src={banner}
-                      alt={`Banner ${banners.length - index}`}
-                      className="imagen-publicidad objetoCentrado1"
-                    />
-                  </div>
-                ))}
-            </Slider>
-          </div>
-        </div>
-
-       
+        ))}
       </div>
     </div>
   );
